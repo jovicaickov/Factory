04bccac028db84181454c63846cd35b7f161e3b2
Jovica Ickov <jovica.ickov@gmail.com>
2020-02-06 20:32:25 +0100

    Initial commit

---
+++ src/main/java/main/factory/Car.java
@@ -0,0 +1,19 @@
+package main.factory;
+
+/**
+ *
+ * @author qa
+ */
+public class Car {
+    
+    String model;
+    int buildYear;
+    int maxSpeed;
+    String color;
+    int minFuel;
+    int maxFuel;
+    int currentFuel;
+    int consumption;
+    
+    
+}
---
+++ src/main/java/main/factory/Main.java
@@ -0,0 +1,38 @@
+package main.factory;
+
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Pocetak");
+        
+        String message = "Ovo je poruka";
+        
+        Car fiat = new Car();
+        fiat.model = "Fiat Punto";
+        fiat.buildYear = 2010;
+        fiat.color = "black";
+        fiat.consumption = 6;
+        fiat.maxSpeed = 180;
+        fiat.currentFuel = 20;
+        fiat.maxFuel = 45;
+        
+        System.out.println("Model: " + fiat.model);
+        System.out.println("Model: " + fiat.model);
+        System.out.println("Model: " + fiat.model);
+        System.out.println("Model: " + fiat.model);
+        System.out.println("Model: " + fiat.model);
+        
+        
+        
+        
+    }
+    
+}
